71d0d135504038eff3e2c7e78a8556bd3d5fa8a8
AbdulRehman <abdulrehman.6ab@gmail.com>
2019-09-22 06:15:47 +0500

    changes in site layout and design

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 let footerFoldIcon = document.querySelectorAll('.footer__allLink__col__top i');
 
-console.log();
 footerFoldIcon.forEach(ele=>{
     ele.addEventListener('click',e=>{
-        console.log(e.target);
 
         if(!(e.target.classList.contains('open'))){
 
             e.target.parentElement.nextElementSibling.classList.remove('hidde');
+            e.target.parentElement.nextElementSibling.previousElementSibling.style.paddingBottom=0;
             e.target.classList.add('open');
             e.target.classList.remove('close');
             e.target.innerHTML="-";
 
         }else if(e.target.classList.contains('open')){
+            e.target.parentElement.nextElementSibling.previousElementSibling.style.paddingBottom='1.5rem';
             e.target.parentElement.nextElementSibling.classList.add('hidde');
                 e.target.classList.remove('open');
             e.target.classList.add('close');
@@ -28,13 +28,87 @@ let searchModal=document.querySelector('.searchModal'),
     searchBoxCloseBtn = document.querySelector('.searchModal__content__close');
 
 searchIcon.addEventListener('click',()=>{
-    console.log('hhh');
     searchModal.classList.remove('closeModal');
 });
 
 
 searchBoxCloseBtn.addEventListener('click',()=>{
-    console.log('aaaa');
     searchModal.classList.add('closeModal');
 });
+
+
+let subscribeModal=document.querySelector('.subscribeModal'),
+    subscribeModalBtn = document.querySelector('.header__top__btn'),
+    subscribeModalCloseBtn = document.querySelector('.subscribeModalCloseBtn');
+
+subscribeModalBtn.addEventListener('click',()=>{
+    subscribeModal.classList.remove('closeModal');
+});
+
+
+subscribeModalCloseBtn.addEventListener('click',()=>{
+    subscribeModal.classList.add('closeModal');
+});
+
+
+
+
+let headerLiClasses=['headerFirstLi','headerSecLi','headerThirdLi','firstBtnOptions','secondBtnOptions','thirdBtnOptions'];
+
+
+headerLiClasses.forEach(ele=>{
+
+    let element = document.querySelector('.'+ele);
+
+    if(element)
+    element.onclick=function (e) {
+        element.children[1].classList.toggle('hide');
+        if(ele==='thirdBtnOptions'){
+            document.querySelector('.'+ele+' img').classList.toggle('rotateUp');
+        }
+    };
+    if(element)
+    window.addEventListener('click',(e)=> {
+        if (e.target !==element.children[0] && e.target !== element.children[1] && e.target!==element.children[0].children[2]) {
+            element.children[1].classList.add('hide');
+        }
+    });
+
+    });
+
+
+document.querySelectorAll('.itemBtnTwo__downIcon').forEach(ele=>{
+    ele.children[0].children[2].onclick=function (e) {
+        ele.children[1].classList.toggle('hide');
+    };
+
+    window.addEventListener('click',(e)=>{
+        if(e.target!=ele.children[0] && e.target!=ele.children[1] && e.target!=ele.children[0].children[2]){
+            ele.children[1].classList.add('hide');
+        }
+    });
+
+});
+
+let nav=document.querySelector('.nav'),
+    nav__close=document.querySelector('.nav__close'),
+    burgerMenu=document.querySelector('.header__top__all__left__navMenu');
+
+
+burgerMenu.onclick=function () {
+    nav.classList.remove('moveNavAway');
+};
+
+nav__close.onclick=function () {
+    nav.classList.add('moveNavAway');
+};
+
+window.addEventListener('click',(e)=>{
+    if(e.target!==nav && e.target!==burgerMenu && e.target!==burgerMenu.children[0]){
+        nav.classList.add('moveNavAway');
+    }
+});
+
+
+
 
